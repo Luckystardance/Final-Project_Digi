8bb18f802225a8e43a4d46a34c830884f3331a9d
Luckystardance <mintliliez@gmail.com>
2016-04-11 12:08:58 -0500

    pro1

---
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/ChangeSceneFoggy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour
+{
+	void OnTriggerEnter(Collider co)
+	{
+		if (co.name == "Player")
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("foggy2");
+	}
+}
